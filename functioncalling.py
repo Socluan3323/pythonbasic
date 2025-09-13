ff9aecef1aff4bf4ce52db500f2fee6235173a02
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-09-13 12:51:26 +0200

    q

---
+++ functioncalling.py
@@ -0,0 +1,5 @@
+def func(x,y):
+    print(x, "is a", y)
+
+def func(name,job,location):
+    print("my name is", name , "my job is", job,"my location is", location)
---
+++ test.py
@@ -0,0 +1,2 @@
+from functioncalling import *
+func(name = "bob", location = "London", job = "developer")
