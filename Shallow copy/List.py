404156761bacf871f84136d3504c50932cd3ef58
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-09-14 10:47:02 +0200

    a

---
+++ Shallow copy/List.py
@@ -0,0 +1,4 @@
+list = [1,2,3]
+list.insert(3,4)
+list.append(10)
+print(list)
---
+++ Shallow copy/Shallow copy.py
@@ -0,0 +1,10 @@
+from Lib import *
+list = [1,2,3,4,5]
+print(list)
+modify_list(list)
+print(list)
+
+string = "Hi"
+print(string)
+modify_str(string)
+print(string)
